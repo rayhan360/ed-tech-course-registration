fe5907ce069d98b31ab41df0045cea67e78cb5a3
Md: Redoan Ahmmad <92013878+rayhan360@users.noreply.github.com>
2023-09-15 02:39:02 +0600

    pricing section complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,22 @@ function App() {
   const [bookMark, setBookMarks] = useState([])
   const [crediHour, setCreditHour] = useState(0)
   const [remainingCredit, setRemainingCredit] = useState(20)
-
+  const [totalPrice, setTotalPrice] = useState(0)
+ 
   const handleBookMarks = bookmark => {
     const isExist = bookMark.find((item) => item.id === bookmark.id)
 
     let creditHour = bookmark.credit_hour;
+    let price = bookmark.price
 
     if (isExist) {
       toast('already added this course')
     } else {
         bookMark.forEach(item => {
           creditHour = creditHour + item.credit_hour;
+          price = price + item.price;
         })
+
 
         const remaining = 20 - creditHour;
         if(creditHour > 20){
@@ -30,6 +34,7 @@ function App() {
         }else{
           setCreditHour(creditHour)
           setRemainingCredit(remaining)
+          setTotalPrice(price)
   
         const newBookMarkCard = [...bookMark, bookmark]
         setBookMarks(newBookMarkCard)
@@ -45,7 +50,7 @@ function App() {
       <Header></Header>
       <div className='md:flex'>
         <Cards handleBookMarks={handleBookMarks}></Cards>
-        <Cart bookMark={bookMark} creditHour={crediHour} remainingCredit={remainingCredit}></Cart>
+        <Cart bookMark={bookMark} creditHour={crediHour} remainingCredit={remainingCredit} totalPrice={totalPrice}></Cart>
       </div>
       <ToastContainer></ToastContainer>
     </>
